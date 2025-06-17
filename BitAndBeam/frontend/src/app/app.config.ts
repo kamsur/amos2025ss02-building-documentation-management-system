13de0f45d42a2f3b6b39a2e8e911b11f236bcdd1
Eugenie Bastiane Djamen <djamenbasty1@gmail.com>
2025-06-17 17:08:59 +0200

    refactor app-config
    Signed-off-by: Eugenie Bastiane Djamen <djamenbasty1@gmail.com>

--- BitAndBeam/frontend/src/app/app.config.ts
+++ BitAndBeam/frontend/src/app/app.config.ts
@@ -1,11 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient()
+  ],
 };
