9b4100b33a01a8e93f2125e513be889d89ada984
Eugenie Bastiane Djamen <djamenbasty1@gmail.com>
2025-06-17 15:11:03 +0200

    Updated App config
    Signed-off-by: Eugenie Bastiane Djamen <djamenbasty1@gmail.com>

--- BitAndBeam/frontend/src/app/app.config.ts
+++ BitAndBeam/frontend/src/app/app.config.ts
@@ -1,12 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http'; // <-- add withInterceptors
 import { routes } from './app.routes';
+import { AuthInterceptor } from './services/auth.interceptor'; // make sure the path is correct
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(
+      withInterceptors([AuthInterceptor]) // <-- REGISTER INTERCEPTOR HERE
+    )
   ],
 };
