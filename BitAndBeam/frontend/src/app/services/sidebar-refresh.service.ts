f38fbcd64297ec2ea3d5bca08296ec8528c1c6e3
Eugenie Bastiane Djamen <djamenbasty1@gmail.com>
2025-06-19 23:02:54 +0200

    Added sidebarservice
    Signed-off-by: Eugenie Bastiane Djamen <djamenbasty1@gmail.com>

---
+++ BitAndBeam/frontend/src/app/services/sidebar-refresh.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class SidebarRefreshService {
+  private refreshSubject = new Subject<void>();
+  refresh$ = this.refreshSubject.asObservable();
+
+  triggerRefresh(): void {
+    this.refreshSubject.next();
+  }
+}
