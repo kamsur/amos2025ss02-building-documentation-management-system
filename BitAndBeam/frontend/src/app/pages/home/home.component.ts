96997f31db73b0f7106e4051235c7ce36267f5ca
Syed Affan Hussain <syedaffan.dev@gmail.com>
2025-07-08 05:16:26 +0200

    Update home.component.ts
    Signed-off-by: Syed Affan Hussain <syedaffan.dev@gmail.com>

--- BitAndBeam/frontend/src/app/pages/home/home.component.ts
+++ BitAndBeam/frontend/src/app/pages/home/home.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { UploadFileComponent } from '../../components/upload-file/upload-file.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [UploadFileComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
