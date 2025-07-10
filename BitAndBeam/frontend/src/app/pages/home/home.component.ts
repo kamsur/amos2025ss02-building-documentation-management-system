8b0601a9de17f3401c2d663aa7e072bbd9f59e49
Masudur Rahaman Kazi <kamsur.rahaman@gmail.com>
2025-07-10 15:34:56 +0200

    login button activity fix
    Signed-off-by: Masudur Rahaman Kazi <kamsur.rahaman@gmail.com>

--- BitAndBeam/frontend/src/app/pages/home/home.component.ts
+++ BitAndBeam/frontend/src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { UploadFileComponent } from '../../components/upload-file/upload-file.component';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [UploadFileComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
