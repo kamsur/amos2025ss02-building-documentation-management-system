3422515fae798fee9dedb203d6b99df77b203e4e
Masudur Rahaman Kazi <kamsur.rahaman@gmail.com>
2025-07-11 22:13:20 +0200

    model corrections
    Signed-off-by: Masudur Rahaman Kazi <kamsur.rahaman@gmail.com>

--- BitAndBeam/ollama/entrypoint.sh
+++ BitAndBeam/ollama/entrypoint.sh
@@ -16,7 +16,7 @@ sleep 10
 ollama pull gemma3:4b
 
 # Create custom named model (overwrite if exists)
-ollama cp gemma3:4b gemma3-4b:latest
+# ollama cp gemma3:4b gemma3-4b:latest
 
 # Create custom model with 8192 context tokens
 # ollama create gemma3-4b-8k -f /app/Modelfile
